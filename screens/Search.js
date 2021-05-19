5241cf1850329a30fe631d5f5f29d1a6dc964259
yezii <1126yezy@gmail.com>
2021-05-19 19:20:21 +0900

    Search

--- screens/Search.js
+++ screens/Search.js
@@ -1,55 +1,116 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import {
+  TouchableOpacity,
+  ActivityIndicator,
+  useWindowDimensions,
+  FlatList,
+  Image,
+} from "react-native";
 import styled from "styled-components/native";
+import { gql, useLazyQuery } from "@apollo/client";
 import DismissKeyboard from "../components/DismissKeyboard";
-import { useTheme } from "../ThemeManager";
 
+const SEARCH_PHOTOS = gql`
+  query searchPhotos($keyword: String!) {
+    searchPhotos(keyword: $keyword) {
+      id
+      file
+    }
+  }
+`;
+const SearchingWrapper = styled.View`
+  flex: 1;
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const MessageContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const MessageText = styled.Text`
+  margin-top: 10px;
+  color: ${(props) => props.theme.textColor};
+  font-weight: 600;
+`;
 const Input = styled.TextInput`
   color: ${(props) => props.theme.textColor};
+  background-color: ${(props) => props.theme.formColor};
+  width: ${(props) => props.width / 1.2}px;
+  padding: 7px 10px;
+  border-radius: 5px;
 `;
 
 export default function Search({ navigation }) {
-  const theme = useTheme();
-  const { register, setValue, watch } = useForm();
+  const numColumns = 3;
+  const { width } = useWindowDimensions();
+  const { register, setValue, handleSubmit } = useForm();
+  const [startQueryFn, { data, loading, called }] = useLazyQuery(SEARCH_PHOTOS);
+
+  const onValid = ({ keyword }) => {
+    startQueryFn({
+      variables: { keyword },
+    });
+  };
+
   const SearchBox = () => (
     <Input
+      width={width}
       placeholder="Search"
       autoCapitalize="none"
       returnKeyLable="Search"
       returnKeyType="search"
       onChangeText={(text) => setValue("keyword", text)}
       autoCorrect={false}
+      onSubmitEditing={handleSubmit(onValid)}
     />
+  );
+
+  const renderItem = ({ item: photo }) => (
+    <TouchableOpacity>
+      <Image
+        source={{ uri: photo.file }}
+        style={{ width: width / numColumns, height: width / numColumns }}
+      />
+    </TouchableOpacity>
   );
 
   useEffect(() => {
     navigation.setOptions({
       headerTitle: SearchBox,
     });
-    register("keyword");
+    register("keyword", { required: true, minLength: 2 });
   }, []);
-
-  console.log(watch());
 
   return (
     <DismissKeyboard>
-      <View
-        style={{
-          backgroundColor: `${theme.mode === "dark" ? "black" : "#fafafa"}`,
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text
-          style={{
-            color: `${theme.mode === "dark" ? "white" : "rgb(38, 38, 38)"}`,
-          }}
-        >
-          Photo
-        </Text>
-      </View>
+      <SearchingWrapper>
+        {loading ? (
+          <MessageContainer>
+            <ActivityIndicator size="large" />
+            <MessageText>Searching...</MessageText>
+          </MessageContainer>
+        ) : null}
+        {!called ? (
+          <MessageContainer>
+            <MessageText>Search by keyword</MessageText>
+          </MessageContainer>
+        ) : null}
+        {data?.searchPhotos !== undefined ? (
+          data?.searchPhotos?.length === 0 ? (
+            <MessageContainer>
+              <MessageText>Could not find anything.</MessageText>
+            </MessageContainer>
+          ) : (
+            <FlatList
+              data={data?.searchPhotos}
+              keyExtractor={(photo) => "" + photo.id}
+              renderItem={renderItem}
+              numColumns={numColumns}
+            />
+          )
+        ) : null}
+      </SearchingWrapper>
     </DismissKeyboard>
   );
 }
